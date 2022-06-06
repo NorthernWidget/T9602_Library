71fba2e9ad989fa52819b3d4cb7ee16cda0c866a
maciejs1982 <77460878+maciejs1982@users.noreply.github.com>
2022-06-06 16:46:42 +0100

    Update T9602.cpp

--- src/T9602.cpp
+++ src/T9602.cpp
@@ -14,38 +14,20 @@ uint8_t T9602::begin(uint8_t ADR_)
 void T9602::updateMeasurements(){
 
   // Relative humidity
-	uint8_t data[2] = {0}; //Array for raw data from device
+	uint8_t data[4] = {0}; //Array for raw data from device
 
 	Wire.beginTransmission(ADR);
-	Wire.write(0x00);  //Read only upper 2 data bytes
+	Wire.write(0);  //Wake up and commence conversions
 	Wire.endTransmission();
 
-	Wire.requestFrom(ADR, 2);
-	for(int i = 0; i < 2; i++) { //Read in raw data
+	Wire.requestFrom(ADR, 4);
+	for(int i = 0; i < 4; i++) { //Read in raw data
 		data[i] = Wire.read();
 	}
-
 	// Convert RH to percent
-	RH = (float)((((data[0] & 0x3F ) << 8) + data[1]) / 16384.0) * 100.0; 
-
-
-  // Temperature
-  
-  // Reset the array to zeros
-	data[0] = 0.;
-	data[1] = 0.;
-
-	Wire.beginTransmission(ADR);
-	Wire.write(0x02);  //Read only from lower 2 data bytes
-	Wire.endTransmission();
-
-	Wire.requestFrom(ADR, 2);
-	for(int i = 0; i < 2; i++) { //Read in raw data
-		data[i] = Wire.read();
-	}
-
-	Temp = (float)((unsigned((data[2] * 64)) + unsigned((data[3] >> 2 ))) / 16384.0) * 165.0 - 40.0;  //Convert Temp
-	
+	RH = (float)(((data[0] & 0x3F ) << 8) + data[1]) / 16384.0 * 100.0; //Convert RH
+	// Temperature
+	Temp = (float)((unsigned)(data[2] * 64) + (unsigned)(data[3] >> 2 )) / 16384.0 * 165.0 - 40.0;  //Convert Temp
 }
 
 float T9602::getHumidity()  //Return humidity in % (realtive)
